5291b491e951a6f68b7e5618ab79e09b8d956892
unknown <mandliksiddhant@gmail.com>
2025-01-16 19:05:50 +0530

    Updates

---
+++ precache-manifest.2e2bdc47bae56fb629572f6259895bfc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e393e2fdee2729c1c9838aadb49b96",
+    "url": "/StemStation/index.html"
+  },
+  {
+    "revision": "3055edca63c70a54f870",
+    "url": "/StemStation/static/css/main.f9613f71.chunk.css"
+  },
+  {
+    "revision": "66a21af02ab252eea7f6",
+    "url": "/StemStation/static/js/2.bba94bfb.chunk.js"
+  },
+  {
+    "revision": "941aa4a4cd3ab784595d",
+    "url": "/StemStation/static/js/3.4f7c6bf9.chunk.js"
+  },
+  {
+    "revision": "b3e2afd360056c7ca5e2",
+    "url": "/StemStation/static/js/4.36a8893b.chunk.js"
+  },
+  {
+    "revision": "3055edca63c70a54f870",
+    "url": "/StemStation/static/js/main.62ca4387.chunk.js"
+  },
+  {
+    "revision": "d9bf209ed454b8a057fc",
+    "url": "/StemStation/static/js/runtime~main.09e896d5.js"
+  },
+  {
+    "revision": "bb683d86058c1d9105637c0f768e62aa",
+    "url": "/StemStation/static/media/aboutpic.bb683d86.jpeg"
+  },
+  {
+    "revision": "06f6f47520213c1656b7097a4d8abb28",
+    "url": "/StemStation/static/media/logo-main.06f6f475.png"
+  },
+  {
+    "revision": "380347bcd6bc40c6d13e3286687b21a4",
+    "url": "/StemStation/static/media/member1.380347bc.jpeg"
+  },
+  {
+    "revision": "cac4f6e5006de8d28add102b962e463f",
+    "url": "/StemStation/static/media/member2.cac4f6e5.jpeg"
+  },
+  {
+    "revision": "09abe20f345953c329eb31a3c4160fe5",
+    "url": "/StemStation/static/media/slide1.09abe20f.jpeg"
+  },
+  {
+    "revision": "432bb54ca9744b46bd2745267f8bd530",
+    "url": "/StemStation/static/media/slide2.432bb54c.jpeg"
+  },
+  {
+    "revision": "0822292d153594c3f19cf3d8e928e114",
+    "url": "/StemStation/static/media/slide3.0822292d.jpeg"
+  },
+  {
+    "revision": "8508e5549f8e6e9efd578aaf32fc34be",
+    "url": "/StemStation/static/media/statspic.8508e554.png"
+  }
+]);
